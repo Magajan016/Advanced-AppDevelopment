b35266920226c4a08f1426ba24448248f1e7f75d
Magajan016 <119098567+Magajan016@users.noreply.github.com>
2024-03-22 15:50:42 +0530

    Add files via upload
    day 02

---
+++ Day2/src/Pages/User/UserLogin.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../../Components/public/Navbar'
+import NavbarDown from '../../Components/public/NavbarDown'
+import Login from './Login'
+import Footer from '../../Components/public/Footer'
+
+function UserLogin() {
+  return (
+    <div>
+        {/* <Navbar/> */}
+        {/* <NavbarDown/> */}
+        <Login/>
+        {/* <Footer/> */}
+    </div>
+  )
+}
+
+export default UserLogin
